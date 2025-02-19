4e1eb7adcf21e74b5a97c3c3a3c7b1c2953248d6
iagoborelli <iago.gomes@aracaju.se.gov.br>
2025-02-19 12:38:26 -0300

    commit da aula 3

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,9 @@ import { env } from './env'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 import { accessInviteLinkRoute } from './routes/acces-invite-link-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
+import { getRankingRoute } from './routes/get-ranking-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -40,9 +43,9 @@ app.register(fastifySwaggerUi, {
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
-/* app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberInvitesCountRoute)
 app.register(getSubscriberRankingPositionRoute)
-app.register(getRankingRoute) */
+app.register(getRankingRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
